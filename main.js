efdc5bd88ff6a9c0283153aedf0f9e1678a76a52
unknown <paln8n7634@gmail.com>
2023-06-26 22:36:31 -0900

    completed

---
+++ main.js
@@ -0,0 +1,11 @@
+var close = document.getElementById("modal-1-content");
+close.style.maxWidth = "0px";
+function ope(){
+    if(close.style.maxWidth == "0px"){
+        close.style.maxWidth = "100%";
+    }
+    else{
+        close.style.maxWidth = "0px";
+    }
+}
+ 
